2db1fd0f94dc7f86502620c6fb63a9490214c89f
Rahul shah <shahmr309@gmail.com>
2024-07-16 21:25:51 +0545

    final

--- my-aap/src/loginComponent/loginComponent.jsx
+++ my-aap/src/loginComponent/loginComponent.jsx
@@ -42,7 +42,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/register', {
+      const response = await fetch('http://localhost:3001/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- my-aap/src/server/server.js
+++ my-aap/src/server/server.js
@@ -1,25 +1,52 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
 const authRoutes = require("../loginRoutes/auth");
 
+// const app = express();
+
+// app.use(bodyParser.json());
+
+// mongoose
+//   .connect("mongodb://localhost:27017")
+//   .then(() => {
+//     console.log("MongoDB connected");
+//   })
+//   .catch((err) => {
+//     console.log("MongoDB connection error:", err);
+//   });
+
+// app.use("http://localhost:3000/register", authRoutes);
+
+// const PORT = 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+//
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 const app = express();
+const port = 3000;
 
+// Middleware to parse JSON data
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Connect to MongoDB
+const mongoURI = "mongodb://127.0.0.1:27017/ehome";
 mongoose
-  .connect("mongodb://localhost:27017/ehome.user")
-  .then(() => {
-    console.log("MongoDB connected");
-  })
+  .connect(mongoURI)
+  .then(() => console.log("Connected to MongoDB"))
   .catch((err) => {
-    console.log("MongoDB connection error:", err);
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1);
   });
+app.use("http://localhost:3001/register/", authRoutes);
 
-app.use("http://localhost:5000/register", authRoutes);
-
-const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
